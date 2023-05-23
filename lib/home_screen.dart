e10e6feb013cd5fe391651d2d741c1c2b1412b77
busrakk <busrakartal160@gmail.com>
2023-05-23 10:05:46 +0300

    create ui on home screen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -33,7 +33,7 @@ class _HomeScreenState extends State<HomeScreen> {
   final TextEditingController _descController = TextEditingController();
 
   // data ekleme
-  Future<void> _addBook(int id) async {
+  Future<void> _addBook() async {
     await SQLHelper.createBook(_titleController.text, _descController.text);
     _refreshData();
   }
@@ -53,8 +53,129 @@ class _HomeScreenState extends State<HomeScreen> {
     _refreshData();
   }
 
+  //
+  void showBottomSheet(int? id) async {
+    if (id != null) {
+      // bir id geldiği için güncelleme
+      final existingBook = _allBooks.firstWhere((book) => book['id'] == id);
+      _titleController.text = existingBook['title'];
+      _descController.text = existingBook['desc'];
+    } else {
+      // id boş bu yüzden yeni kayıt ekleme
+      _titleController.clear();
+      _descController.clear();
+    }
+
+    // alttan ekran çıkartma
+    showModalBottomSheet(
+      context: context,
+      elevation: 0,
+      isScrollControlled: true,
+      // builder : her bir elemanı temsil eder)
+      builder: (context) => Container(
+        padding: EdgeInsets.only(
+            top: 30,
+            left: 15,
+            right: 15,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 50),
+        child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              TextField(
+                controller: _titleController,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(), hintText: 'Title'),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              TextField(
+                controller: _descController,
+                maxLines: 4, // 4 satırdan oluşsun
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(), hintText: 'Description'),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () async {
+                    if (id == null) {
+                      // yeni kitap ekleme
+                      await _addBook();
+                    } else {
+                      // güncelleme
+                      await _updateBook(id);
+                    }
+                    _titleController.clear();
+                    _descController.clear();
+
+                    Navigator.of(context).pop(); // son açılan ekranı kapat
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      id == null ? "Add a Book" : "Update a Book",
+                      style: const TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              )
+            ]),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("MP Library"),
+      ),
+      body: _isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: _allBooks.length, // toplam kaç data var
+              itemBuilder: (context, index) => Card(
+                margin: const EdgeInsets.all(5),
+                child: ListTile(
+                  title: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: Text(
+                      _allBooks[index]['title'],
+                      style: const TextStyle(fontSize: 25),
+                    ),
+                  ),
+                  subtitle: Text(
+                    _allBooks[index]['desc'],
+                  ),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                          onPressed: () {
+                            showBottomSheet(_allBooks[index]['id']);
+                          },
+                          icon: const Icon(Icons.edit, color: Colors.indigo)),
+                      IconButton(
+                          onPressed: () {
+                            _deleteBook(_allBooks[index]['id']);
+                          },
+                          icon: const Icon(Icons.delete, color: Colors.red)),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => showBottomSheet(null),
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
